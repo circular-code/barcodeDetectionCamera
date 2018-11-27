a5efdcd78b89b1c4cdb85108904a6dafc70c903e
circular-code <stephanos@malikotsinas.de>
2018-11-27 10:18:36 +0100

    removed canvas, and other useless stuff

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
+// jshint -W119
 'use strict';
 
 var video = document.querySelector('video');
-var canvas = window.canvas = document.querySelector('canvas');
-canvas.width = 480;
-canvas.height = 360;
-
-var button = document.querySelector('button');
-button.onclick = function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-};
-
 var constraints = { audio: false, video: { facingMode: { exact: "environment" } } };
 
 function handleSuccess(stream) {
@@ -25,7 +15,6 @@ function handleError(error) {
 
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 
-// jshint -W119
 window.onload = () => {
     video.addEventListener('click', function(e) {
 
